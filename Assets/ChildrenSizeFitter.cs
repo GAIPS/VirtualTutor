575698cbbdf852ac83ff37ebd24a41e5b200c5dd
Unknown <ricardo.e.p.rodrigues@gmail.com>
2017-05-15 17:51:01 +0100

    Responsive Layout updates
    Testing done on Android. Kinda mhe.

---
+++ Assets/ChildrenSizeFitter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Children size fitter.
+/// 
+/// Size the owner of this script to the size of all the children of a given
+/// game object.
+/// </summary>
+[ExecuteInEditMode]
+public class ChildrenSizeFitter : MonoBehaviour {
+
+    public RectTransform parentTransform;
+
+    public Transform childrenHolder;
+
+    public float multiplier = 1.0f;
+	
+    // Update is called once per frame
+    void Update() {
+        if (childrenHolder && parentTransform) {
+            float maxY = 0;
+            foreach (Transform child in childrenHolder) {
+                RectTransform childRect = child as RectTransform;
+                if (childRect) {
+                    float maxYLocal = System.Math.Abs(child.localPosition.y) + System.Math.Abs(childRect.rect.yMax);
+                    if (maxY < maxYLocal) {
+                        maxY = maxYLocal;
+                    }
+                }
+            }
+
+            parentTransform.sizeDelta = new Vector2(parentTransform.sizeDelta.x,
+                                                    maxY * multiplier);
+        }
+    }
+}
---
+++ Assets/SameHeight.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SameHeight : MonoBehaviour {
+
+    public RectTransform copyTransform;
+    public RectTransform targetTransform;
+
+    private float previousHeight;
+	
+    // Update is called once per frame
+    void Update() {
+        if (!copyTransform || !targetTransform) {
+            return;
+        }
+
+        if (previousHeight != copyTransform.rect.height) {
+            previousHeight = copyTransform.rect.height;
+            targetTransform.sizeDelta = new Vector2(targetTransform.sizeDelta.x,
+                                                     previousHeight); 
+        }
+    }
+}
