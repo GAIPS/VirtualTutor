7d57901c9a7ed34e06d005daadb8b7ed46233c00
Ricardo <ricardo.g.pereira@ist.utl.pt>
2018-03-13 17:21:01 +0000

    initial bubblesystem package added

---
+++ Assets/BubbleSystem/Text/TextModifier.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BubbleSystem
+{
+    public class TextModifier : Modifier
+    {
+        private Dictionary<Data, TextData> dictionary = new Dictionary<Data, TextData>();
+
+        private TextData textData;
+
+        private void Start()
+        {
+            SetDictionary("Text/", "Jsons/text");
+        }
+
+        public TextData SelectText(Data data)
+        {
+            try
+            {
+                return dictionary.Where(key => data.intensity <= key.Key.intensity && key.Key.emotion.Equals(data.emotion)).OrderBy(key => key.Key.intensity).First().Value;
+            }
+            catch
+            {
+                throw new KeyNotFoundException("Emotion " + data.emotion + " with intensity " + data.intensity + " and reason " + data.reason + " not defined.");
+            }
+        }
+
+        protected override void Add(Data data)
+        {
+            dictionary.Add(data, textData);
+        }
+
+        protected override void Set<T>(T data, string attribute, bool defaultData)
+        {
+            var type = typeof(T);
+            if (type.Equals(typeof(Texture2D)) && attribute.Equals("color"))
+            {
+                textData.colorData.SetColor((List<object>)Convert.ChangeType(data, typeof(List<object>)));
+            }
+            else if (type.Equals(typeof(Font)) && attribute.Equals("font"))
+            {
+                textData.font = (Font)Convert.ChangeType(data, typeof(Font));
+            }
+            else if (type.Equals(typeof(float)) && attribute.Equals("size"))
+            {
+                textData.size = (float)Convert.ChangeType(data, typeof(float));
+            }
+        }
+    }
+}
---
+++ Assets/BubbleSystem/Utility/ImageManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ImageManager : MonoBehaviour {
+
+    protected void StopCoroutineWithCheck(IEnumerator coroutine)
+    {
+        if (coroutine != null)
+        {
+            StopCoroutine(coroutine);
+        }
+    }
+}
