a7c25bb9ae7631185dd16ab7cd3690f24109703c
Ricardo <ricardo.g.pereira@ist.utl.pt>
2018-04-21 18:36:06 +0100

    test folder created

---
+++ Assets/BubbleAnimatorTest/Manager.cs
@@ -0,0 +1,16 @@
+﻿using BubbleSystem;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour {
+
+    public BubbleSystemManager manager;
+	
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            manager.Speak("Maria", BubbleSystem.Emotion.Happiness.ToString(), 1.0f, new string[] { "hi" }, 5f);
+        }
+	}
+}
