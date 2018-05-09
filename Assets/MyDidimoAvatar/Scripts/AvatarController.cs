3d3d3b95d7dee3b285e26e14dc2626bf8991b59a
Ricardo Silva <ricardo.filipe.silva@hotmail.com>
2018-05-09 19:12:00 +0100

    Lerped mood transition; mood parameter adjustments; mood dampener
    - Added lerping to the mood intensity values on mood transition to achieve less brisk changes.
    - Tweaked some of the default parameters of  every mood to achieve a more credible result.
    - Added a mood dampener to the avatar parameters class, that can be used to reduce the intensity of mood animations, if necessary. (fixes #4)

--- Assets/MyDidimoAvatar/Scripts/AvatarController.cs
+++ Assets/MyDidimoAvatar/Scripts/AvatarController.cs
@@ -13,9 +13,9 @@ public partial class AvatarController : MonoBehaviour
     {
         animator = GetComponent<Animator>();
         parameters = GetComponent<AvatarParameters>();
-
     }
-    void Start()
+
+    private void Start()
     {
         float[][] paramPreset = AvatarParameters.Presets.Neutral();
         parameters.setAllParameters<AnimatorParams>(paramPreset[0]);
@@ -24,7 +24,9 @@ public partial class AvatarController : MonoBehaviour
         StartCoroutine("NoddingRoutine");
         StartCoroutine("PassiveRoutine");
     }
-    void FixedUpdate(){
+
+    private void FixedUpdate()
+    {
         //AnimationClip c = getClipByName("Head_Nod");
         //Debug.Log("CURRENT STATE INFO: [NAME = " + c.name + ", LENGHT = " + c.length + "]");
         //currentBaseState = animator.GetCurrentAnimatorStateInfo(3);
@@ -40,24 +42,29 @@ public partial class AvatarController : MonoBehaviour
             case EmotionalState.NEUTRAL:
                 preset = AvatarParameters.Presets.Neutral();
                 break;
+
             case EmotionalState.HAPPINESS:
-                if(intensity > LOWINTENSITYTHRESHOLD)
+                if (intensity > LOWINTENSITYTHRESHOLD)
                     preset = AvatarParameters.Presets.HappinessHigh();
                 else
                     preset = AvatarParameters.Presets.HappinessLow();
                 break;
+
             case EmotionalState.SADNESS:
                 if (intensity > LOWINTENSITYTHRESHOLD)
                     preset = AvatarParameters.Presets.SadnessHigh();
                 else
                     preset = AvatarParameters.Presets.SadnessLow();
                 break;
+
             case EmotionalState.FEAR:
                 preset = AvatarParameters.Presets.Fear();
                 break;
+
             case EmotionalState.SURPRISE:
                 preset = AvatarParameters.Presets.Surprise();
                 break;
+
             case EmotionalState.ANGER:
             case EmotionalState.DISGUST:
             default:
@@ -67,26 +74,31 @@ public partial class AvatarController : MonoBehaviour
 
         parameters.setAllParameters<AnimatorParams>(preset[0]);
         parameters.setAllParameters<ControllerParams>(preset[1]);
-        parameters.setParameter(AnimatorParams.MOOD_INTENSITY, intensity);
+
+        StartCoroutine(MoodTransition(animator.GetFloat("Mood Intensity"), intensity * parameters.getMoodDampenerValue()));
 
         animator.SetInteger("Mood", (int)mood);
     }
+
     public void ExpressEmotion(EmotionalState expression, float intensity)
     {
         parameters.setParameter(AnimatorParams.EXPRESSION_INTENSITY, intensity);
         animator.SetInteger("Expression", (int)expression);
         animator.SetTrigger("Express");
     }
+
     public void DoNodding(NodState nodState)
     {
         animator.SetInteger("Nod Style", (int)nodState);
         animator.SetTrigger("Nod");
     }
+
     public void DoTalking(TalkState talkState)
     {
         animator.SetInteger("Talk State", (int)talkState);
         animator.SetTrigger("Talk");
     }
+
     public void DoGazing(GazeState gazeState)
     {
         animator.SetInteger("Direction", (int)gazeState);
@@ -103,6 +115,7 @@ public partial class AvatarController : MonoBehaviour
     {
         parameters.setParameter(param, value);
     }
+
     public void setParameter(ControllerParams param, float value)
     {
         // Gaze frequency is set for both "at" and "back" animations
@@ -115,11 +128,11 @@ public partial class AvatarController : MonoBehaviour
             parameters.setParameter(param, value);
     }
 
-    IEnumerator NoddingRoutine()
+    private IEnumerator NoddingRoutine()
     {
         float length, frequency;
         while (true)
-        {   
+        {
             if (animator.gameObject.activeSelf && animator.GetBool("Listening"))
             {
                 length = parameters.nodLength / parameters.getParameter(AnimatorParams.NOD_SPEED);
@@ -131,14 +144,14 @@ public partial class AvatarController : MonoBehaviour
                 else
                 {
                     DoNodding(NodState.NOD_END);
-
                 }
-                yield return new WaitForSeconds(length);    
+                yield return new WaitForSeconds(length);
             }
-            yield return null;      
+            yield return null;
         }
     }
-    IEnumerator PassiveRoutine()
+
+    private IEnumerator PassiveRoutine()
     {
         //float length, gazelength;
         while (true)
@@ -146,46 +159,55 @@ public partial class AvatarController : MonoBehaviour
             yield return new WaitForSeconds(UnityEngine.Random.Range(3, 5)); //delay
             if (!animator.GetBool("Listening") && animator.GetInteger("Talk State") == 0)
             {
-                switch (UnityEngine.Random.Range(1,15))
+                switch (UnityEngine.Random.Range(1, 15))
                 { //Testing some random expressive motions
                     case 1:
                         animator.SetInteger("Expression2", 0);
                         animator.SetTrigger("Express2");
                         break;
+
                     case 2:
                     case 9:
                         animator.SetInteger("Expression2", 1);
                         animator.SetTrigger("Express2");
                         break;
+
                     case 3:
                     case 10:
                         animator.SetInteger("Expression2", 2);
                         animator.SetTrigger("Express2");
                         break;
+
                     case 4:
                         animator.SetInteger("Move Eyes State", 1);
                         animator.SetTrigger("Move Eyes");
                         break;
+
                     case 5:
                         animator.SetInteger("Move Eyes State", 2);
                         animator.SetTrigger("Move Eyes");
                         break;
+
                     case 6:
                         animator.SetInteger("Move Eyes State", 3);
                         animator.SetTrigger("Move Eyes");
                         break;
+
                     case 7:
                         animator.SetInteger("Move Eyes State", 4);
                         animator.SetTrigger("Move Eyes");
                         break;
+
                     case 8:
                         animator.SetInteger("Move Eyes State", 5);
                         animator.SetTrigger("Move Eyes");
                         break;
+
                     case 11:
                     case 12:
                         ExpressEmotion(EmotionalState.SURPRISE, 0.1f);
                         break;
+
                     default:
                         break;
                 }
@@ -193,10 +215,22 @@ public partial class AvatarController : MonoBehaviour
         }
     }
 
+    private IEnumerator MoodTransition(float start, float end)
+    {
+        float t = 0.0f;
+        while (t <= 1.0f)
+        {
+            parameters.setParameter(AnimatorParams.MOOD_INTENSITY, Mathf.Lerp(start, end, t));
+            t += 0.5f * Time.deltaTime;
+            yield return null;
+        }
+    }
+
     public void isListening(bool state)
     {
         animator.SetBool("Listening", state);
     }
+
     public AvatarParameters getParameters()
     {
         return parameters;
