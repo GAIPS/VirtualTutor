6dc9a6052b59a03a3ac0830df50d08f7b502bd29
RicardoEPRodrigues <ricardo.e.p.rodrigues@gmail.com>
2018-06-07 19:06:30 +0100

    Added Automated build process for Main. Build process now checks for errors.

---
+++ Assets/VT/Main/Scripts/Main/Editor/MainBuildProcess.cs
@@ -0,0 +1,118 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class MainBuildProcess : MonoBehaviour
+{
+    private static string[] mainLevels = {"Assets/VT/Main/Scenes/VT_Main.unity"};
+
+    [MenuItem("VT Tools/Main Build Tools/Build All", false, 0)]
+    public static void BuildGame()
+    {
+        // Get filename.
+        //  string path = EditorUtility.SaveFolderPanel("Choose Location of Built Game", "", "Main");
+        string path = GetProjectPath() + "/Build/Main/";
+
+        if (!BuildGameForPlatform(mainLevels, path, BuildTarget.Android)) return;
+        if (!BuildGameForPlatform(mainLevels, path, BuildTarget.StandaloneWindows64)) return;
+        if (!BuildGameForPlatform(mainLevels, path, BuildTarget.StandaloneLinux64)) return;
+        if (!BuildGameForPlatform(mainLevels, path, BuildTarget.StandaloneOSXIntel64)) return;
+
+        Debug.Log("Completed all builds to " + path);
+        EditorUtility.RevealInFinder(path);
+    }
+
+    [MenuItem("VT Tools/Main Build Tools/Build For Android", false, 1)]
+    public static void BuildGameForAndroid()
+    {
+        string path = GetProjectPath() + "/Build/Main/";
+        BuildGameForPlatform(mainLevels, path, BuildTarget.Android);
+        Debug.Log("Completed Mac build to " + path);
+    }
+
+    [MenuItem("VT Tools/Main Build Tools/Build For Windows", false, 2)]
+    public static void BuildGameForWindows()
+    {
+        string path = GetProjectPath() + "/Build/Main/";
+        BuildGameForPlatform(mainLevels, path, BuildTarget.StandaloneWindows64);
+        Debug.Log("Completed Windows build to " + path);
+    }
+
+    [MenuItem("VT Tools/Main Build Tools/Build For Linux", false, 3)]
+    public static void BuildGameForLinux()
+    {
+        string path = GetProjectPath() + "/Build/Main/";
+        BuildGameForPlatform(mainLevels, path, BuildTarget.StandaloneLinux64);
+        Debug.Log("Completed Linux build to " + path);
+    }
+
+    [MenuItem("VT Tools/Main Build Tools/Build For Mac", false, 4)]
+    public static void BuildGameForMac()
+    {
+        string path = GetProjectPath() + "/Build/Main/";
+        BuildGameForPlatform(mainLevels, path, BuildTarget.StandaloneOSXIntel64);
+        Debug.Log("Completed Mac build to " + path);
+    }
+
+    private static bool BuildGameForPlatform(string[] levels, string path, BuildTarget target)
+    {
+        string buildPath = path;
+        string extention;
+        switch (target)
+        {
+            case BuildTarget.Android:
+                buildPath += "Android/";
+                extention = "apk";
+                break;
+            case BuildTarget.StandaloneWindows64:
+                buildPath += "Windows/";
+                extention = "exe";
+                break;
+            case BuildTarget.StandaloneLinux64:
+                buildPath += "Linux/";
+                extention = "x86_64";
+                break;
+            case BuildTarget.StandaloneOSXIntel64:
+                buildPath += "Mac/";
+                extention = "app";
+                break;
+            default:
+                Debug.LogWarning("Building for an unsupported plartform.");
+                buildPath += target + "/";
+                extention = "run";
+                break;
+        }
+
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions
+        {
+            scenes = levels,
+            locationPathName = buildPath + "VirtualTutoring." + extention,
+            target = target,
+            options = BuildOptions.None
+        };
+        // Build player.
+        string result = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        if (!string.IsNullOrEmpty(result))
+        {
+            return false;
+        }
+
+        // Copy files here
+        return true;
+    }
+
+    [MenuItem("VT Tools/Main Build Tools/Clean All Builds", false, 40)]
+    public static void ClearBuilds()
+    {
+        string path = GetProjectPath() + "/Build/Main/";
+        FileUtil.DeleteFileOrDirectory(path);
+        Debug.Log("Deleted " + path);
+    }
+
+    private static string GetProjectPath()
+    {
+        DirectoryInfo projectDirectory = new DirectoryInfo(Directory.GetCurrentDirectory());
+        string projectPath = projectDirectory.FullName.Replace('\\', '/');
+        return projectPath;
+    }
+}
--- Assets/VT/Main/Scripts/Preview/Editor/PreviewBuildProcess.cs
+++ Assets/VT/Main/Scripts/Preview/Editor/PreviewBuildProcess.cs
@@ -13,9 +13,9 @@ public class PreviewBuildProcess : MonoBehaviour
         //  string path = EditorUtility.SaveFolderPanel("Choose Location of Built Game", "", "Preview");
         string path = GetProjectPath() + "/Build/Preview/";
 
-        BuildGameForPlatform(previewLevels, path, BuildTarget.StandaloneWindows64);
-        BuildGameForPlatform(previewLevels, path, BuildTarget.StandaloneLinux64);
-        BuildGameForPlatform(previewLevels, path, BuildTarget.StandaloneOSXIntel64);
+        if (!BuildGameForPlatform(previewLevels, path, BuildTarget.StandaloneWindows64)) return;
+        if (!BuildGameForPlatform(previewLevels, path, BuildTarget.StandaloneLinux64)) return;
+        if (!BuildGameForPlatform(previewLevels, path, BuildTarget.StandaloneOSXIntel64)) return;
 
         Debug.Log("Completed all builds to " + path);
         EditorUtility.RevealInFinder(path);
@@ -25,30 +25,30 @@ public class PreviewBuildProcess : MonoBehaviour
     public static void BuildGameForWindows()
     {
         string path = GetProjectPath() + "/Build/Preview/";
-        BuildGameForPlatform(previewLevels, path, BuildTarget.StandaloneWindows64);
-        Debug.Log("Completed Windows build to " + path);
+        if (BuildGameForPlatform(previewLevels, path, BuildTarget.StandaloneWindows64))
+            Debug.Log("Completed Windows build to " + path);
     }
 
     [MenuItem("VT Tools/Preview Build Tools/Build For Linux", false, 2)]
     public static void BuildGameForLinux()
     {
         string path = GetProjectPath() + "/Build/Preview/";
-        BuildGameForPlatform(previewLevels, path, BuildTarget.StandaloneLinux64);
-        Debug.Log("Completed Linux build to " + path);
+        if (BuildGameForPlatform(previewLevels, path, BuildTarget.StandaloneLinux64))
+            Debug.Log("Completed Linux build to " + path);
     }
 
     [MenuItem("VT Tools/Preview Build Tools/Build For Mac", false, 3)]
     public static void BuildGameForMac()
     {
         string path = GetProjectPath() + "/Build/Preview/";
-        BuildGameForPlatform(previewLevels, path, BuildTarget.StandaloneOSXIntel64);
-        Debug.Log("Completed Mac build to " + path);
+        if (BuildGameForPlatform(previewLevels, path, BuildTarget.StandaloneOSXIntel64))
+            Debug.Log("Completed Mac build to " + path);
     }
 
-    private static void BuildGameForPlatform(string[] levels, string path, BuildTarget target)
+    private static bool BuildGameForPlatform(string[] levels, string path, BuildTarget target)
     {
         string buildPath = path;
-        string extention = string.Empty;
+        string extention;
         switch (target)
         {
             case BuildTarget.StandaloneWindows64:
@@ -63,12 +63,29 @@ public class PreviewBuildProcess : MonoBehaviour
                 buildPath += "Mac/";
                 extention = "app";
                 break;
+            default:
+                Debug.LogWarning("Building for an unsupported plartform.");
+                buildPath += target + "/";
+                extention = "run";
+                break;
         }
 
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions
+        {
+            scenes = levels,
+            locationPathName = buildPath + "Preview." + extention,
+            target = target,
+            options = BuildOptions.None
+        };
         // Build player.
-        BuildPipeline.BuildPlayer(levels, buildPath + "Preview." + extention, target, BuildOptions.None);
+        string result = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        if (!string.IsNullOrEmpty(result))
+        {
+            return false;
+        }
 
         CopyBuildAssetsForPlatform(path, target);
+        return true;
     }
 
     [MenuItem("VT Tools/Preview Build Tools/Copy All Build Assets", false, 20)]
@@ -124,7 +141,7 @@ public class PreviewBuildProcess : MonoBehaviour
                 os += "Mac/";
                 break;
         }
-        
+
         CheckDirectory(buildPath);
 
         // Copy example file
