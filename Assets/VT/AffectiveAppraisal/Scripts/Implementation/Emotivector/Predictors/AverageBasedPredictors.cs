5474e714da8d916fb93ff2f296465bd1bb50cf49
RicardoEPRodrigues <ricardo.e.p.rodrigues@gmail.com>
2018-06-15 01:38:38 +0100

    Added predictors using average and differentiation.
    The following predictors were added:
    
    * Moving Average
    * Weighted Moving Average
    * Exponential Moving Average
    * Additive First Derivative Predictor
    * First Derivative Only Predictor
    * Additive Second Derivative Predictor

---
+++ Assets/VT/AffectiveAppraisal/Scripts/Implementation/Emotivector/Predictors/AverageBasedPredictors.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class MovingAveragePredictor : IPredictor
+{
+    public int ValuesWindow { get; set; }
+
+    public MovingAveragePredictor(int valuesWindow = 5)
+    {
+        ValuesWindow = valuesWindow;
+    }
+
+    public float Predict(List<float> values, List<float> predictions, float? desiredValue)
+    {
+        // if values are empty assume half value between 0 and 1 (0.5)
+        if (values == null || values.Count == 0)
+        {
+            return 0.5f;
+        }
+
+        float average = 0;
+        int size = ValuesWindow > values.Count ? values.Count : ValuesWindow;
+        for (int i = 0; i < size; i++)
+        {
+            average += values[values.Count - 1 - i] / size;
+        }
+
+        return average;
+    }
+}
+
+public class WeightedMovingAveragePredictor : IPredictor
+{
+    public float[] Weights { get; set; }
+
+    public WeightedMovingAveragePredictor() : this(new float[5] {0.5f, .2f, .1f, .1f, .1f})
+    {
+    }
+
+    public WeightedMovingAveragePredictor(float[] weights)
+    {
+        Weights = weights;
+    }
+
+    public float Predict(List<float> values, List<float> predictions, float? desiredValue)
+    {
+        // if values are empty assume half value between 0 and 1 (0.5)
+        if (values == null || values.Count == 0)
+        {
+            return 0.5f;
+        }
+
+        float average = 0;
+        int size = Weights.Length;
+        float extra = 0;
+        if (Weights.Length > values.Count)
+        {
+            size = values.Count;
+            float diff = Weights.Length - values.Count;
+            for (int i = 0; i < diff; i++)
+            {
+                extra += Weights[Weights.Length - 1 - i];
+            }
+
+            extra /= Weights.Length - diff;
+        }
+
+        for (int i = 0; i < size; i++)
+        {
+            average += values[values.Count - 1 - i] * (Weights[i] + extra);
+        }
+
+        return average;
+    }
+}
+
+/// <summary>
+/// Exponential moving average predictor.
+/// </summary>
+/// <remarks>
+/// Because it is not looking at a window of values it makes heavy use of the predictions list.
+/// </remarks>
+public class ExponentialMovingAveragePredictor : IPredictor
+{
+    public float Predict(List<float> values, List<float> predictions, float? desiredValue)
+    {
+        float value;
+        // if values are empty assume half value between 0 and 1 (0.5)
+        if (values == null || values.Count == 0)
+        {
+            value = 0.5f;
+            return value;
+        }
+
+        value = values[values.Count - 1];
+
+        float lastPrediction;
+        // if there are no previous predictions assume the same value
+        if (predictions == null || predictions.Count == 0)
+            lastPrediction = value;
+        else
+            lastPrediction = predictions[predictions.Count - 1];
+
+        return (value - lastPrediction) * (2f / (values.Count + 1)) + lastPrediction;
+    }
+}
--- Assets/VT/AffectiveAppraisal/Scripts/Implementation/Emotivector/Predictors/MartinhoSimplePredictor.cs
+++ Assets/VT/AffectiveAppraisal/Scripts/Implementation/Emotivector/Predictors/MartinhoSimplePredictor.cs
@@ -2,47 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 
-public class MathUtils
-{
-    public static float Exogenous(float value, float expected)
-    {
-        return DeltaSquared(value, expected);
-    }
-    
-    public static float ExogenousScaled(float value, float expected)
-    {
-        return DeltaSquared(value, expected) * 10;
-    }
-
-    public static float ExogenousInverted(float value, float expected)
-    {
-        return 1 - DeltaSquared(value, expected);
-    }
-
-    public static float Endogenous(float value, float expected, float searched)
-    {
-        float deltaS = DeltaSquared(value, searched);
-        float deltaSExpected = DeltaSquared(expected, searched);
-
-        return deltaSExpected - deltaS;
-    }
-
-    public static float DeltaSquared(float a, float b)
-    {
-        return (float) Math.Pow(a - b, 2);
-    }
-
-    public static float DeltaAbs(float a, float b)
-    {
-        return (float) Math.Abs(a - b);
-    }
-}
-
-public interface IPredictor
-{
-    float Predict(List<float> values, List<float> predictions, float? desiredValue = null);
-}
-
 public class MartinhoSimplePredictor : IPredictor
 {
     public float Predict(List<float> values, List<float> predictions, float? desiredValue)
