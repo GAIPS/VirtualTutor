de73482ad728ca44d4dab8a005a4baab9c3aa87b
RicardoEPRodrigues <ricardo.e.p.rodrigues@gmail.com>
2018-10-11 15:09:23 +0100

    Restructuring of placement of code.
    Imported pieces of code or code that is not the focus of VT are now at Packages.

---
+++ Assets/Packages/HookControl/Scripts/Delegates.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace HookControl
+{
+    public delegate void IntFunc(int value);
+
+    public delegate void BoolFunc(bool value);
+
+    public delegate void StringFunc(string value);
+
+    public delegate void VoidFunc();
+
+    public delegate void GameObjectFunc(GameObject gameObj);
+
+    public delegate void FloatFunc(float value);
+}
---
+++ Assets/Packages/HookControl/Scripts/Hook.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+namespace HookControl
+{
+    public class Hook : MonoBehaviour
+    {
+    }
+}
